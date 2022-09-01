a38f140c580ed05a2893dc3ff53654a26765b377
Gozi1 <snwabiani@gmail.com>
2022-09-01 13:39:05 +0000

    Looping and dividing numbers 100-200

---
+++ Loopy-Lighthouse.js
@@ -0,0 +1,15 @@
+
+for (let i = 100; i <= 200; i++) {
+  if(i % 12 === 0){
+    console.log("LoopyLighthouse")
+  }
+  else if(i % 3 === 0){
+    console.log("Loopy")
+  }
+  else if(i % 4 === 0){
+    console.log("Lighthouse")
+  }
+  else{
+    console.log(i)
+  }
+}
