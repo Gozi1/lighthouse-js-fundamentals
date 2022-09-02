d90d4b124fe0154a4e697aa88e2c5fa96efc69e8
Gozi1 <snwabiani@gmail.com>
2022-09-02 19:46:59 +0000

    iterating  in different wats through a list

---
+++ ingredients.js
@@ -0,0 +1,19 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+console.log("\n While Loop \n")
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+// Write a for loop that prints out the contents of ingredients:
+console.log("\n For Loop \n")
+for (let i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+console.log("\n Backwards For Loop \n")
+for (let i = ingredients.length-1; i >= 0; i--) {
+  console.log(ingredients[i]);
+}
